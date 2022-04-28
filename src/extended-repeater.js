92422e455e7db82f7af9401f09a3b37e0c4800ac
Yauheni Oshkin <garaz777@gmail.com>
2022-04-28 19:28:04 +0300

    feat: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let { repeatTimes, separator, addition, additionRepeatTimes, additionSeparator } = options;
+  if (!repeatTimes) return str + plusAddition(addition, additionRepeatTimes, additionSeparator);
+  let res = '';
+  for (let i = 1; i <= repeatTimes; i++) {
+    if (!separator) {
+      separator = '+';
+    }
+    i < repeatTimes
+      ? res = res + str + plusAddition(addition, additionRepeatTimes, additionSeparator) + separator
+      : res = res + str + plusAddition(addition, additionRepeatTimes, additionSeparator);
+  }
+  return res
+}
+function plusAddition(addition, additionRepeatTimes, additionSeparator) {
+  let resAddition = '';
+  if (!addition && !additionRepeatTimes && !additionSeparator) return resAddition;
+  if (!additionRepeatTimes && typeof additionSeparator !== 'number') return addition;
+  for (let i = 1; i <= additionRepeatTimes; i++) {
+    if (!additionSeparator) {
+      additionSeparator = '|'
+    }
+    i < additionRepeatTimes
+      ? resAddition = resAddition + addition + additionSeparator
+      : resAddition = resAddition + addition;
+  }
+  return resAddition;
 }
 
 module.exports = {
