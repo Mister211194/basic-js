6106013cae0bb67ec14b3fc25c067ddd34100ff4
Yauheni Oshkin <garaz777@gmail.com>
2022-04-29 23:08:30 +0300

    feat: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('\'arr\' parameter must be an instance of the Array!')
+  }
+  const res = arr.slice()
+  res.forEach((item, i) => {
+    if (res[i] === '--double-next') {
+      typeof res[i + 1] === 'number' && res[i + 1]
+        ? res.splice(i, 1, res[i + 1])
+        : res.splice(i, 1)
+    }
+    else if (res[i] === '--double-prev') {
+      typeof res[i - 1] === 'number' && res[i - 1]
+        ? res.splice(i, 1, res[i - 1])
+        : res.splice(i, 1)
+    }
+    else if (res[i] === '--discard-next') {
+      typeof res[i + 1] === 'number' && res[i + 1]
+        ? res.splice(i, 2, null)
+        : res.splice(i, 1)
+    }
+    else if (res[i] === '--discard-prev') {
+      typeof res[i - 1] === 'number' && res[i - 1]
+        ? res.splice(i - 1, 2, null)
+        : res.splice(i, 1)
+    }
+  })
+
+  return res.filter(item => item !== null);
 }
 
 module.exports = {
