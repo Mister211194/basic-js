e701ccc37506eabf512e0e4a3506aa30af21cedd
Yauheni Oshkin <garaz777@gmail.com>
2022-04-29 12:56:20 +0300

    feat: mini-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const clone = JSON.parse(JSON.stringify(matrix));
+  const res = clone.map(element => element.map(item => item = 0));
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i - 1]) {
+        if (matrix[i - 1][j - 1] || matrix[i - 1][j] || matrix[i - 1][j + 1]) res[i][j] += 1;
+      }
+      if (matrix[i][j - 1] || matrix[i][j + 1]) res[i][j] += 1;
+      if (matrix[i + 1]) {
+        if (matrix[i + 1][j - 1] || matrix[i + 1][j] || matrix[i + 1][j + 1]) res[i][j] += 1;
+      }
+    }
+  }
+  return res;
 }
 
 module.exports = {
